09072d8364bab69e93482c4deff65faee0978ad1
Bobjoerules <39201587+bobjoerules@users.noreply.github.com>
2023-09-27 14:32:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const colorInput = document.getElementById("favcolor");
+
+colorInput.addEventListener("input", function () {
+  // Get the selected color value from the input element
+  const selectedColor = colorInput.value;
+
+  // Update the background color of the entire page
+  document.body.style.backgroundColor = selectedColor;
+});
