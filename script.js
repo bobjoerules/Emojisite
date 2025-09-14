7fccaab5daf973a9a7c3d1a676c9ed23b5faad99
bobjoerules <yo.bob.joe.rules@gmail.com>
2025-09-14 16:27:14 -0700

    Made changes that make broken combos not be able to be made (broke my brain)

--- script.js
+++ script.js
@@ -1,17 +1,187 @@
 const colorInput = document.getElementById("favcolor");
 const glassesbtn = document.getElementById("sunglassesbtn")
+const glasses = document.getElementById("glasses")
 const noaccessbtn = document.getElementById("noaccess")
 const reye = document.getElementById("reye")
 const leye = document.getElementById("leye")
+const disr = document.getElementById("disr")
+const disl = document.getElementById("disl")
+const winkl = document.getElementById("winkl")
+const grinbtn = document.getElementById("grinbtn")
+const griningbtn = document.getElementById("griningbtn")
+const accessories = document.getElementById('accessories')
+const mouth = document.getElementById('mouth')
+let mouthblockerl = 0
+let mouthblockerr = 0
 
 glassesbtn.addEventListener("click", () => {
-  reye.style.display = "none";
-  leye.style.display = "none";
+  if (glasses.src.split("/").pop() === 'Sunglasses.png'){
+    glasses.src='images/nothing.png'
+    reye.style.display = "block";
+    leye.style.display = "block";
+  }else{
+    glasses.src='images/Face Accessories/Sunglasses.png'
+    reye.style.display = "none";
+    leye.style.display = "none";
+  }
 });
-noaccess.addEventListener("click", () => {
+
+noaccessbtn.addEventListener("click", () => {
+  glasses.src='images/nothing.png'
+  accessories.src='images/nothing.png'
   reye.style.display = "block";
   leye.style.display = "block";
 });
+
+document.getElementById('joytearsbtn').onclick = () => {
+  console.log(accessories)
+  if(accessories.src.split('/').pop() === 'Joy%20Tears.png'){
+    accessories.src='images/nothing.png'
+  }else{
+    accessories.src='images/Face Accessories/Joy Tears.png'
+    accessories.style.zIndex = 6;
+    glasses.style.zIndex = 5;
+  }
+};
+
+document.getElementById('ROFLtearsbtn').onclick = () => {
+  if(accessories.src.split('/').pop() === 'ROFL%20Tears.png'){
+    accessories.src='images/nothing.png'
+    accessories.style.zIndex = 6;
+    glasses.style.zIndex = 5;
+  }else{
+    accessories.src='images/Face Accessories/ROFL Tears.png'
+    accessories.style.zIndex = 5;
+    glasses.style.zIndex = 6;
+  }
+}
+
+document.getElementById('sweat').onclick = () => {
+  if(accessories.src.split('/').pop() === 'Sweat.png'){
+    accessories.src='images/nothing.png'
+  }else{
+    accessories.src='images/Face Accessories/Sweat.png'
+    accessories.style.zIndex = 6;
+    glasses.style.zIndex = 5;
+  }
+}
+
+grinbtn.onclick = () => {
+  mouth.src='images/Mouth/Grin Mouth.png';
+  disableeyes()
+}
+griningbtn.onclick = () => {
+  mouth.src='images/Mouth/Grinning Mouth.png'
+  disableeyes()
+}
+
+document.getElementById('heartsmilebtn').onclick = () => {
+  mouth.src='images/Mouth/Heart Smile.png'
+  enableeyes()
+}
+document.getElementById('largesmilebtn').onclick = () => {
+  mouth.src='images/Mouth/Large Smile.png'
+  enableeyes()
+}
+document.getElementById('smallsmilebtn').onclick = () => {
+  mouth.src='images/Mouth/Small Smile.png'
+    enableeyes()
+}
+document.getElementById('yumbtn').onclick = () => {
+  mouth.src='images/Mouth/Yum Mouth.png'
+  enableeyes()
+}
+document.getElementById('nonemouthbtn').onclick = () => {
+  mouth.src='images/nothing.png'
+  enableeyes()
+}
+document.getElementById('sunglassessmilebtn').onclick = () => {
+  mouth.src='images/Mouth/Sunglasses Smile.png'
+  enableeyes()
+}
+
+document.getElementById('disl').onclick = () => {
+  leye.src='images/Left Eyes/Disappointed L Eye.png';
+  if(mouthblockerl === 0){
+    mouthblockerl ++;
+    disablemouth()
+  }
+}
+document.getElementById('disr').onclick = () => {
+  reye.src='images/Right Eyes/Disappointed R Eye.png'
+  if(mouthblockerr === 0){
+    mouthblockerr ++;
+    disablemouth()
+  }
+}
+document.getElementById('winkl').onclick = () => {
+  leye.src='images/Left Eyes/Wink L Eye.png';
+  if(mouthblockerl === 0){
+    mouthblockerl ++;
+    disablemouth()
+  }
+}
+
+function disableeyes() {
+  winkl.disabled = true;
+  winkl.style.backgroundColor = 'red';
+  winkl.style.opacity = 0.5;
+  winkl.style.cursor = 'default';
+  disr.disabled = true;
+  disr.style.backgroundColor = 'red';
+  disr.style.opacity = 0.5;
+  disr.style.cursor = 'default';
+  disl.disabled = true;
+  disl.style.backgroundColor = 'red';
+  disl.style.opacity = 0.5;
+  disl.style.cursor = 'default';
+}
+
+function enableeyes() {
+  winkl.disabled = false;
+  winkl.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+  winkl.style.opacity = 1;
+  winkl.style.cursor = 'pointer';
+  disr.disabled = false;
+  disr.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+  disr.style.opacity = 1;
+  disr.style.cursor = 'pointer';
+  disl.disabled = false;
+  disl.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+  disl.style.opacity = 1;
+  disl.style.cursor = 'pointer';
+}
+
+function disablemouth() {
+  grinbtn.disabled = true;
+  grinbtn.style.backgroundColor = 'red';
+  grinbtn.style.opacity = 0.5;
+  grinbtn.style.cursor = 'default';
+  griningbtn.disabled = true;
+  griningbtn.style.backgroundColor = 'red';
+  griningbtn.style.opacity = 0.5;
+  griningbtn.style.cursor = 'default';
+}
+function enablemouthr(){
+  mouthblockerr = 0
+  enablemouth()
+}
+function enablemouthl(){
+  mouthblockerl = 0
+  enablemouth()
+}
+function enablemouth() {
+  if ((mouthblockerr === 0) && (mouthblockerl === 0)){
+    grinbtn.disabled = false;
+    grinbtn.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+    grinbtn.style.opacity = 1;
+    grinbtn.style.cursor = 'pointer';
+    griningbtn.disabled = false;
+    griningbtn.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+    griningbtn.style.opacity = 1;
+    griningbtn.style.cursor = 'pointer';
+  }
+}
 
 colorInput.addEventListener("input", function () {
   // Get the selected color value from the input element
@@ -22,7 +192,6 @@ colorInput.addEventListener("input", function () {
 });
 
 dragElement(document.getElementById("settings"));
-
 
 function dragElement(elmnt) {
   let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
